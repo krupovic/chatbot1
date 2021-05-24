31825f90ab2f77a53b2d34f9939c70556d60cbcb
krupovic <petrkruppa@gmail.com>
2021-05-24 17:46:52 +0300

    minor bugfix

--- chatbot1.py
+++ chatbot1.py
@@ -140,7 +140,7 @@ while True:
                 if 'бот кто маф' in txt.lower() and config['who-is-m'] == 1:
                     b = api.messages.getConversationMembers(peer_id = prid)
                     numb = randint(0, (b['count']-1-len(b['groups'])))
-                    send(f'Могу смело утверждать, что маф - [id{b['profiles'][numb]['id']} | {b['profiles'][numb]['first_name']} ]', prid, True)
+                    send(f'Могу смело утверждать, что маф - [id{b["profiles"][numb]["id"]} | {b["profiles"][numb]["first_name"]} ]', prid, True)
                        
                 if txt.lower() == 'return cfg':
                     send(json.dumps(config, indent = 4), prid, True)
